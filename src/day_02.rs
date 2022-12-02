eba155d30e37c5e039d4b24fb10ce170773510b2
fractalart <FractalArt@users.noreply.github.com>
2022-12-02 18:27:35 +0100

    Day 02 - Task 2

--- src/day_02.rs
+++ src/day_02.rs
@@ -3,7 +3,7 @@
 //! This module contains the solution of the [second day's challenges](https://adventofcode.com/2022/day/2).
 
 /// The solution to task 1 of day 2.
-pub fn day_02(data: &[String]) -> u32 {
+pub fn day_02_1(data: &[String]) -> u32 {
     data.iter()
         .map(|line| line.split(' ').collect::<Vec<_>>())
         .map(|letters| match (letters[0], letters[1]) {
@@ -20,6 +20,24 @@ pub fn day_02(data: &[String]) -> u32 {
         .sum()
 }
 
+/// The solution to task 2 of day 2.
+pub fn day_02_2(data: &[String]) -> u32 {
+    data.iter()
+        .map(|line| line.split(' ').collect::<Vec<_>>())
+        .map(|letters| match (letters[0], letters[1]) {
+            ("A", "X") => 3,
+            ("A", "Y") => 4,
+            ("A", "Z") => 8,
+            ("B", "X") => 1,
+            ("B", "Y") => 5,
+            ("B", "Z") => 9,
+            ("C", "X") => 2,
+            ("C", "Y") => 6,
+            _ => 7,
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -27,6 +45,12 @@ mod tests {
     #[test]
     fn test_day_02_1() {
         let input = vec!["A Y".to_string(), "B X".to_string(), "C Z".to_string()];
-        assert_eq!(day_02(&input), 15);
+        assert_eq!(day_02_1(&input), 15);
+    }
+
+    #[test]
+    fn test_day_02_2() {
+        let input = vec!["A Y".to_string(), "B X".to_string(), "C Z".to_string()];
+        assert_eq!(day_02_2(&input), 12);
     }
 }
--- tests/day_02.rs
+++ tests/day_02.rs
@@ -3,6 +3,8 @@ use aoc2022::{self, read_data};
 #[test]
 fn test_day_02() {
     let data = read_data::<String, _>("data/day_02.txt").unwrap();
-    let task_2 = aoc2022::day_02::day_02(&data);
-    assert_eq!(task_2, 13924);
+    let task_1 = aoc2022::day_02::day_02_1(&data);
+    assert_eq!(task_1, 13924);
+    let task_2 = aoc2022::day_02::day_02_2(&data);
+    assert_eq!(task_2, 13448);
 }
