a171d0d00c90d641919781d7f739dd69146a435a
fractalart <FractalArt@users.noreply.github.com>
2022-12-06 20:11:24 +0100

    Day 06 - Task 2

--- src/day_06.rs
+++ src/day_06.rs
@@ -1,23 +1,22 @@
 //! # Advent of Code 2022 - Day 6
 //!
 //! This module contains the solution of the [second day's challenges](https://adventofcode.com/2022/day/6).
-use itertools::Itertools;
 use std::collections::HashSet;
 
 /// The solution to task 1 of day 4.
-pub fn day_06(data: &str) -> usize {
-    data.chars()
-        .tuple_windows::<(_, _, _, _)>()
+pub fn day_06(data: &str, take: usize) -> usize {
+    data.as_bytes()
+        .windows(take)
         .enumerate()
-        .find_map(|(index, tuple)| {
-            if HashSet::from([tuple.0, tuple.1, tuple.2, tuple.3]).len() == 4 {
-                Some(index)
+        .map(|(index, window)| (index, window.iter().collect::<HashSet<_>>()))
+        .find_map(|(index, set)| {
+            if set.len() == take {
+                Some(index + take)
             } else {
                 None
             }
         })
         .unwrap()
-        + 4
 }
 
 #[cfg(test)]
@@ -26,10 +25,19 @@ mod tests {
 
     #[test]
     fn test_day_06_1() {
-        assert_eq!(day_06("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
-        assert_eq!(day_06("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
-        assert_eq!(day_06("nppdvjthqldpwncqszvftbrmjlhg"), 6);
-        assert_eq!(day_06("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
-        assert_eq!(day_06("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+        assert_eq!(day_06("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4), 7);
+        assert_eq!(day_06("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), 5);
+        assert_eq!(day_06("nppdvjthqldpwncqszvftbrmjlhg", 4), 6);
+        assert_eq!(day_06("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4), 10);
+        assert_eq!(day_06("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4), 11);
+    }
+
+    #[test]
+    fn test_day_06_2() {
+        assert_eq!(day_06("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14), 19);
+        assert_eq!(day_06("bvwbjplbgvbhsrlpgdmjqwftvncz", 14), 23);
+        assert_eq!(day_06("nppdvjthqldpwncqszvftbrmjlhg", 14), 23);
+        assert_eq!(day_06("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14), 29);
+        assert_eq!(day_06("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14), 26);
     }
 }
--- tests/day_06.rs
+++ tests/day_06.rs
@@ -9,6 +9,8 @@ fn test_day_06() {
         .unwrap()
         .read_to_string(&mut data)
         .unwrap();
-    let task_1 = aoc2022::day_06::day_06(&data);
+    let task_1 = aoc2022::day_06::day_06(&data, 4);
     assert_eq!(task_1, 1658);
+    let task_2 = aoc2022::day_06::day_06(&data, 14);
+    assert_eq!(task_2, 2260);
 }
